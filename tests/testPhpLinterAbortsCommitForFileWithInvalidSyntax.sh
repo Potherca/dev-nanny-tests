f584e6b5d3bd8014331d85cc3e856b21bbbfc080
Ben Peachey <potherca@gmail.com>
2015-04-20 01:31:45 +0200

    Makes regular expression for PHP Lint check more robust
    There seems to be a difference in which message is used for syntax errors between various versions of PHP.
    The regular expression that is supposed to catch the message has been improved to take this into account.

--- tests/testPhpLinterAbortsCommitForFileWithInvalidSyntax.sh
+++ tests/testPhpLinterAbortsCommitForFileWithInvalidSyntax.sh
@@ -3,4 +3,4 @@ composer install --prefer-source --no-interaction
 echo '<?php foo' > foo.php
 git add foo.php
 
-sOutput=$(git commit -m 'Adds broken PHP file' 2>&1) || echo "${sOutput}" | grep --quiet 'Parse error: parse error in foo.php on line 2'
+sOutput=$(git commit -m 'Adds broken PHP file' 2>&1) || echo "${sOutput}" | grep --quiet -E 'Parse error: *(syntax|parse) error(, unexpected end of file)? in foo.php on line 2'
