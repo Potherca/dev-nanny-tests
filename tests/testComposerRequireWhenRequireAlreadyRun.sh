6cbcdd91342c9fa43176a2d63d3ae064f6be1f48
Ben Peachey <potherca@gmail.com>
2015-04-19 23:29:55 +0200

    Adds test that validates composer require works when called repeatedly.

---
+++ tests/testComposerRequireWhenRequireAlreadyRun.sh
@@ -0,0 +1,4 @@
+git init
+source './tests/helperRemoveRequiredSectionFromComposerJson.sh'
+composer require 'dev-nanny/dev-nanny'
+composer require 'dev-nanny/dev-nanny' | grep --quiet 'Installed dev-nanny pre-commit hook'
